5c898e8bb24f101ad3591c9e7228171a561f985a
anurajchavan1 <148971670+anurajchavan1@users.noreply.github.com>
2025-05-02 21:24:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const path = require('path');
 require("dotenv").config();
 
 const connectDB = require("./config/db");
@@ -17,13 +16,8 @@ connectDB();
 
 app.use("/", authRoutes); // handles /login and /signup
 app.post("/analyze", analyzeWebsiteHandler);
-// serve everything in ../public as static assets
-app.use(express.static(path.join(__dirname, '../public')));
 app.use('/api/users', require('./routes/userRoutes'));
-// if no API route matches, send back public/index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
-});
+
 
 app.listen(PORT, () =>
   console.log(`✅ Server running on http://localhost:${PORT}`)
